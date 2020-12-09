e1b1a15e7c96e84ea0aa25c28aa83b60067b85b9
= <=>
2020-12-09 12:51:20 +0000

    fix(Script): :bug: Fix Script Test for Pester 5

--- OmniPSTemplates/Templates/NewScript/test/Script.T.ps1
+++ OmniPSTemplates/Templates/NewScript/test/Script.T.ps1
@@ -1,14 +1,16 @@
-$ScriptName = '<%=$PLASTER_PARAM_ScriptName%>.ps1'
-$ScriptDirectory = "$PSScriptRoot\..\src"
-$ScriptPath = "$ScriptDirectory\$ScriptName"
-
 Describe 'Top-level Script Tests' {
+    BeforeAll {
+        $ScriptName = '<%=$PLASTER_PARAM_ScriptName%>.ps1'
+        $ScriptDirectory = "$PSScriptRoot\..\src"
+        [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+        $ScriptPath = "$ScriptDirectory\$ScriptName"
+    }
     It 'Passes Test-ScriptFileInfo' {
         Test-ScriptFileInfo -Path $ScriptPath
-        $? | Should Be $true
+        $? | Should -Be $true
     }
     It 'Is a single file' {
         $Files = Get-ChildItem $ScriptDirectory -Recurse
-        $Files.Count | Should be 1
+        $Files.Count | Should -be 1
     }
 }
