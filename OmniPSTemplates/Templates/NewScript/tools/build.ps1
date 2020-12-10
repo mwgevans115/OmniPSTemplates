cc5759e216dbe7b08da565b6e067dc06e1bd8ced
Mark Evans <mark@madspaniels.co.uk>
2020-12-10 09:03:57 +0000

    fix(Script): Updated build script to install PSDepend

--- OmniPSTemplates/Templates/NewScript/tools/build.ps1
+++ OmniPSTemplates/Templates/NewScript/tools/build.ps1
@@ -4,6 +4,7 @@ param(
     [ValidateSet('Build','Test','Clean','Analyze','Publish')]
     [string[]]$Task = 'Build'
 )
+Install-Module PSDepend -Scope CurrentUser -Force
 Invoke-PSDepend -Path "$PSScriptRoot\build.requirements.psd1" -Install -Import -Force
 Set-BuildEnvironment -Path "$PSScriptRoot\.." -Force
 Invoke-psake -buildFile "$PSScriptRoot\build.psake.ps1" -taskList $Task -Verbose:$VerbosePreference
